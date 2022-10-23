fdf7cc99ffa144b59050a00b78bf32c65690463b
ninnasvanstrom <108125472+ninnasvanstrom@users.noreply.github.com>
2022-10-23 18:48:02 +0200

    Update app.js

--- script/app.js
+++ script/app.js
@@ -1,6 +1,6 @@
 //VARIABLES
 //Sections
-const mainPage = document.getElementById("main-page");
+const mainPage = document.querySelector("main");
 
 //Navbar
 const hamburgerMenu = document.getElementById("hamburger-menu");
@@ -100,80 +100,5 @@ function hideProjectTwoName () {
 
 
 
-// //Function declarations-------------------------
-
-    //on scroll
-
-// //Functions-------------------------------------
-
-//     //Logo
-   
-
-//     // function scrollRotateStar() {
-//     //     let purpleStar = document.getElementById("star-image");
-//     //     purpleStar.style.transform = "rotate(" + window.pageYOffset/2 + "deg)";
-//     // }
-
-//     // // Kanske dumt att alla dom här stjärnorna har olika namn , den ovan också 
-//     // // Den här funkar inte (classname)..?
-//     // function scrollRotateStars() {
-//     //     let footerStars = document.getElementsByClassName("star_image");
-//     //     footerStars.style.transform = "rotate(" + window.pageYOffset/2 + "deg)";
-//     // }
-
-//     // function navbarNavigation() {
-//     //     let navbarLine = document.getElementById('green-line');
-//     //     navbarLine.style.color = 'blue';
-//     // }
-
-
-
-
-
-// //Tutorial:
-// //https://www.youtube.com/watch?v=RsPWEmfOQdU&ab_channel=WEBCIFAR
-
-
-// //Variables
-// const sections = document.querySelectorAll('section');
-// const navLi = document.querySelectorAll('nav .menu-container ul li');
-
-// //Functions---------------------------
-// //Target the sections when scrolling the page
-// window.addEventListener('scroll', () => {
-//     let current = ''; //current section
-    
-//     sections.forEach(section => {
-//         const sectionTop = section.offsetTop; //.offsetTop returns the top position in pxls of the section
-//         const sectionHeight = section.clientHeight; //.clientHeight measures the inner height of the sections in terms of pixels including the CSS properties like padding 
-
-//         //window.pageYOffset returns the number of pixels the document is currently scrolled along the vertical axis (that is, up or down)
-//         //.getAttribute('id') returns the id value of the element, in this case the section
-//         //if the pageYOffset value is greater than or equal to sectionTop, then set the current variable to that sections id-attribute
-//         if(window.pageYOffset >= (sectionTop)) {  //to make the nav change sooner
-//             current = section.getAttribute("id");
-//         }
-//     })
-
-//     //log the current section you're browsing at
-//     //console.log(current);
-
-//     //for each li-element..
-//     navLi.forEach(li => {
-//         //..remove the .active class if they have it:
-//         li.classList.remove('active');
-
-//         //..if the li-element contains a class that matches the current variable..
-//         if(li.classList.contains(current)) {
-//             //..add the active class to that li-element
-//            li.classList.add('active'); 
-//         };
-//     })
-// })
-
-
-// //Console message 
-// console.log('What are you doing here??? :)')
-
-
-// // - sectionHeight / 3
+// Console message 
+console.log("She's brilliant, hire her.")
